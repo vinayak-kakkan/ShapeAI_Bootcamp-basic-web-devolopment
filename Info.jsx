ce4eda7385ccf29128935596ebc81ad9b8e999cf
vinayak-kakkan <85050456+vinayak-kakkan@users.noreply.github.com>
2021-06-27 03:27:25 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>python & java script bootcamp</h1>
+      <p4>bootcamp based on python and java script</p4>
+    </div>
+  );
+              
+}
+export default Info;
